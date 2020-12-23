e5e9092275d2448bd3294e99fc3148ae0fe940a3
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2020-12-23 09:26:48 -0600

    Initial Commit

---
+++ AudioVizualizerAK5/AudioVizualizerAK5App.swift
@@ -0,0 +1,17 @@
+//
+//  AudioVizualizerAK5App.swift
+//  AudioVizualizerAK5
+//
+//  Created by Matt Pfeiffer on 12/23/20.
+//
+
+import SwiftUI
+
+@main
+struct AudioVizualizerAK5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioVizualizerAK5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AudioVizualizerAK5
+//
+//  Created by Matt Pfeiffer on 12/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
